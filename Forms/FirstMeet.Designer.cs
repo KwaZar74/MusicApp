20568af3e9d1f2330f5648e3518263ebb1c4c48c
ilya <ilya@DESKTOP-J5JTT4F>
2023-08-25 21:35:22 +0300

    Авторизация

--- Forms/FirstMeet.Designer.cs
+++ Forms/FirstMeet.Designer.cs
@@ -32,9 +32,13 @@
             button1 = new Button();
             button2 = new Button();
             checkBox1 = new CheckBox();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             linkLabel1 = new LinkLabel();
             label1 = new Label();
-            button3 = new Button();
+            label5 = new Label();
             SuspendLayout();
             // 
             // openFileDialog1
@@ -45,7 +49,7 @@
             // button1
             // 
             button1.Font = new Font("Consolas", 8.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(42, 58);
+            button1.Location = new Point(41, 139);
             button1.Name = "button1";
             button1.Size = new Size(136, 30);
             button1.TabIndex = 0;
@@ -56,7 +60,7 @@
             // button2
             // 
             button2.Font = new Font("Consolas", 8.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(42, 123);
+            button2.Location = new Point(41, 228);
             button2.Name = "button2";
             button2.Size = new Size(136, 28);
             button2.TabIndex = 1;
@@ -67,53 +71,104 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(42, 244);
+            checkBox1.Location = new Point(41, 292);
             checkBox1.MaximumSize = new Size(200, 400);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(18, 17);
             checkBox1.TabIndex = 2;
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Enabled = false;
+            button3.Font = new Font("Consolas", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(119, 377);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 56);
+            button3.TabIndex = 5;
+            button3.Text = "Начать";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 76);
+            label2.MaximumSize = new Size(300, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(296, 60);
+            label2.TabIndex = 6;
+            label2.Text = "Выберите файл с ключем аунтефикации, чтобы получить доступ к предыдущим настройкам (кроме музыки)";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(41, 185);
+            label3.MaximumSize = new Size(300, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(274, 40);
+            label3.TabIndex = 7;
+            label3.Text = "Или получите новый ключ, что иметь настройки нового пользователя";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(77, -1);
+            label4.Name = "label4";
+            label4.Size = new Size(238, 46);
+            label4.TabIndex = 8;
+            label4.Text = "Авторизация";
+            // 
             // linkLabel1
             // 
             linkLabel1.AutoSize = true;
-            linkLabel1.Location = new Point(143, 312);
+            linkLabel1.LinkArea = new LinkArea(72, 83);
+            linkLabel1.Location = new Point(65, 292);
+            linkLabel1.MaximumSize = new Size(300, 0);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(90, 20);
-            linkLabel1.TabIndex = 3;
+            linkLabel1.Size = new Size(267, 65);
+            linkLabel1.TabIndex = 9;
             linkLabel1.TabStop = true;
-            linkLabel1.Text = "exampl.com";
-            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            linkLabel1.Text = "Подтверждаю своё ознакомление и согласие с пользовательским соглашением example.com";
+            linkLabel1.UseCompatibleTextRendering = true;
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked_1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(70, 241);
-            label1.MaximumSize = new Size(300, 0);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.Green;
+            label1.Location = new Point(183, 143);
             label1.Name = "label1";
-            label1.Size = new Size(266, 60);
-            label1.TabIndex = 4;
-            label1.Text = "Подтверждаю своё ознакомление и согласие с пользовательским соглашением";
+            label1.Size = new Size(66, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Готово";
             // 
-            // button3
+            // label5
             // 
-            button3.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button3.Location = new Point(134, 386);
-            button3.Name = "button3";
-            button3.Size = new Size(119, 43);
-            button3.TabIndex = 5;
-            button3.Text = "Начать";
-            button3.UseVisualStyleBackColor = true;
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label5.ForeColor = Color.Green;
+            label5.Location = new Point(183, 231);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Готово";
             // 
             // FirstMeet
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(400, 500);
-            Controls.Add(button3);
+            ClientSize = new Size(400, 491);
+            Controls.Add(label5);
             Controls.Add(label1);
             Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
             Controls.Add(checkBox1);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -131,8 +186,12 @@
         private Button button1;
         private Button button2;
         private CheckBox checkBox1;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
         private LinkLabel linkLabel1;
         private Label label1;
-        private Button button3;
+        private Label label5;
     }
 }
--- Forms/FirstMeet.cs
+++ Forms/FirstMeet.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -45,6 +47,16 @@ namespace MusicApp.Forms
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.example.com") { UseShellExecute = true });
         }
     }
 }
